208422e9fc577e4d31dd68795c7e340f9514a90f
Michael Simons <michael.simons@neo4j.com>
2019-06-04 16:31:32 +0200

    Extract the Neo4j Java Driver Spring Boot Starter from SDN-RX.

---
+++ neo4j-java-driver-spring-boot-autoconfigure/src/main/java/org/neo4j/driver/springframework/boot/autoconfigure/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains the properties and automatic configuration for Neo4js Java Driver.
+ *
+ * @author Michael J. Simons
+ */
+package org.neo4j.driver.springframework.boot.autoconfigure;
