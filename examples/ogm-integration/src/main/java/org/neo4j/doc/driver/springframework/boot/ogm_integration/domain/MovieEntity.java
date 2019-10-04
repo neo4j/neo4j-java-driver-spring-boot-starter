75b2ba6289f481a01f8106bd8f5b6dc0cf0db023
Michael Simons <michael.simons@neo4j.com>
2019-10-04 15:21:35 +0200

    Add OGM support.
    Boot 2.2 now uses OGM 3.2, compatible with 4.0 driver.

---
+++ examples/ogm-integration/src/main/java/org/neo4j/doc/driver/springframework/boot/ogm_integration/domain/MovieEntity.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2019 "Neo4j,"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.doc.driver.springframework.boot.ogm_integration.domain;
+
+import org.neo4j.ogm.annotation.GeneratedValue;
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.springframework.data.annotation.PersistenceConstructor;
+
+@NodeEntity("Movie")
+public class MovieEntity {
+
+	@Id @GeneratedValue
+	private Long id;
+
+	private final String title;
+
+	@PersistenceConstructor
+	MovieEntity(Long id, String title) {
+		this.id = id;
+		this.title = title;
+	}
+
+	public MovieEntity(String title) {
+		this(null, title);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+}
